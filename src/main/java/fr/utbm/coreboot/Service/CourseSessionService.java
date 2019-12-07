b48e057a40fe044cda141edeeba4791cb44cb510
AFFOGBOLO Giovanni <affogbologiovanni@gmail.com>
2019-12-07 08:44:18 +0100

    services fait

--- src/main/java/fr/utbm/coreboot/Service/CourseSessionService.java
+++ src/main/java/fr/utbm/coreboot/Service/CourseSessionService.java
@@ -1,60 +1,41 @@
-//package fr.utbm.coreboot.Service;
-//
-//import fr.utbm.schoolmaven.coreboot.entity.CourseSession;
-//import org.springframework.stereotype.Service;
-//import fr.utbm.schoolmaven.coreboot.repository.HibernateCourseSessionDAO;
-//import java.text.ParseException;
-//import java.text.SimpleDateFormat;
-//import java.util.ArrayList;
-//import java.util.Date;
-//import java.util.List;
-//import java.util.Locale;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import javax.transaction.Transactional;
-//
-//
-//
-//@Transactional
-//@Service
-//public class CourseSessionService {
-//    @Autowired
-//    private HibernateCourseSessionDAO hibernateCoursSessionDao;
-//
-//    public List<CourseSession> all(){
-//        return hibernateCoursSessionDao.all();
-//    }
-//
-//    public void addSession(CourseSession c) {
-//        this.hibernateCoursSessionDao.addSession(c);
-//    }
-//
-//     public void updateSession(CourseSession c) {
-//        this.hibernateCoursSessionDao.updateSession(c);
-//    }
-//
-//    public CourseSession findById(Integer id) {
-//        return this.hibernateCoursSessionDao.findById(id);
-//    }
-//
-//    public void removeSession(Integer id) {
-//        this.hibernateCoursSessionDao.removeSession(id);
-//    }
-//
-//
-// public List<CourseSession> getCourseSessionFilter(String city, String  title, String dateSession) throws ParseException
-//    {
-//        List<CourseSession> courseSessions;
-//        courseSessions = hibernateCoursSessionDao.getCourseSessionByLocationTitleDate(city, title, dateSession);
-//        return courseSessions;
-//    }
-//
-//
-//    public List<CourseSession> getCourseSessionByLocationTitle(String city,String title)
-//    {
-//        List<CourseSession> courseSessions;
-//        courseSessions = hibernateCoursSessionDao.getCourseSessionByLocationTitle(city,title);
-//        return courseSessions;
-//    }
-//
-//
-//}
+package fr.utbm.coreboot.Service;
+
+import fr.utbm.coreboot.Entity.CourseSession;
+import org.springframework.stereotype.Service;
+import fr.utbm.coreboot.Repository.CourseSessionRepository;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import org.springframework.beans.factory.annotation.Autowired;
+import javax.transaction.Transactional;
+
+@Transactional
+@Service
+public class CourseSessionService {
+
+    @Autowired
+    private CourseSessionRepository courseSessionRepository;
+
+    public List<CourseSession> all() {
+        return (List<CourseSession>) courseSessionRepository.findAll();
+    }
+
+    public void addCourseSession(CourseSession cs) {
+        this.courseSessionRepository.save(cs);
+    }
+
+    public CourseSession findCourseSessionClientById(int id) {
+        return this.courseSessionRepository.findById(id).get();
+    }
+
+    public void updateCourseSession(CourseSession cs) {
+        this.courseSessionRepository.save(cs);
+    }
+
+    public void deleteCourseSession(int id) {
+        this.courseSessionRepository.deleteById(id);
+    }
+}
--- src/main/java/fr/utbm/coreboot/Service/LocationService.java
+++ src/main/java/fr/utbm/coreboot/Service/LocationService.java
@@ -1,40 +1,37 @@
-//package fr.utbm.coreboot.Service;
-//
-//import fr.utbm.schoolmaven.coreboot.entity.Location;
-//
-//import fr.utbm.schoolmaven.coreboot.entity.Location;
-//import fr.utbm.schoolmaven.coreboot.repository.HibernateLocationDAO;
-//import java.util.List;
-//import javax.transaction.Transactional;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//
-//@Transactional
-//@Service
-//public class LocationService {
-//
-//    @Autowired
-//    private HibernateLocationDAO hibernateLocationDao;
-//
-//    public LocationService(){
-//        hibernateLocationDao = new HibernateLocationDAO();
-//    }
-//
-//    public List<Location> all(){
-//        return hibernateLocationDao.all();
-//    }
-//
-//
-//    public void addLocation(Location c) {
-//        this.hibernateLocationDao.addLocation(c);
-//    }
-//
-//    public void updateLocation(Location c) {
-//        this.hibernateLocationDao.updateLocation(c);
-//    }
-//
-//    public void removeLocation(int id) {
-//        this.hibernateLocationDao.deleteLocation(id);
-//    }
-//}
+package fr.utbm.coreboot.Service;
+
+import fr.utbm.coreboot.Entity.Location;
+
+import fr.utbm.coreboot.Repository.LocationRepository;
+import java.util.List;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Transactional
+@Service
+public class LocationService {
+
+    @Autowired
+    private LocationRepository locationRepository;
+
+    public List<Location> all() {
+        return (List<Location>) locationRepository.findAll();
+    }
+
+    public void addLocation(Location l) {
+        this.locationRepository.save(l);
+    }
+
+    public Location findLocationById(int id) {
+        return this.locationRepository.findById(id).get();
+    }
+
+    public void updateLocation(Location l) {
+        this.locationRepository.save(l);
+    }
+
+    public void deleteLocation(int id) {
+        this.locationRepository.deleteById(id);
+    }
+}
